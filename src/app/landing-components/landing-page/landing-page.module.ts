b20f1fccf862ba0d344d644a1415ae0d32e42b42
mandeepb29 <mandeepb29@gmail.com>
2019-11-10 02:41:52 +0530

    mandeep 4th commit

--- src/app/landing-components/landing-page/landing-page.module.ts
+++ src/app/landing-components/landing-page/landing-page.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from 'src/app/navbar/navbar.component';
-import { FooterComponent } from 'src/app/footer/footer.component';
+
 
 @NgModule({
-  declarations: [NavbarComponent,FooterComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
